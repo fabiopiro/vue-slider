058220a59d4038e953ef6c82727bc578bab40b4e
fabiopiro <piroddifabio01@gmail.com>
2021-05-18 15:10:12 +0200

    scheletro

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var app = new Vue (
+    {
+        el: "#root",
+        
+        data: {
+            images: [
+                img,
+                img,
+                img,
+                img,
+                img,
+            ],
+        },
+
+        methods: {
+
+        }
+    }
+)
